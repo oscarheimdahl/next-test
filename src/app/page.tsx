dc71fc5db009a765177161b875b9e60cedf8062d
oscarheimdahl <o.heimdahl@gmail.com>
2023-07-21 11:21:19 +0200

    install pre commit script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <nav className='flex flex-col gap-2 m-2'>
+    <nav className="flex flex-col gap-2 m-2">
       <Link href={'create-new'}>
         <button>Create New</button>
       </Link>
       <Link href={'create-new'}>
         <button>Create New Template</button>
-            </Link>
+      </Link>
       <Link href={'create-new'}>
         <button>Import</button>
       </Link>
